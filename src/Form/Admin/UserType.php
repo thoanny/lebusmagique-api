946336c413bf72dacbe9fa6898afcdec186ab8a1
Anthony Destenay <anthony.destenay@gmail.com>
2023-06-02 20:00:32 +0200

    Added User edit

---
+++ src/Form/Admin/UserType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form\Admin;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse e-mail',
+                'attr' => ['class' => 'input input-bordered'],
+                'label_attr' => ['class' => 'label-text'],
+            ])
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Roles',
+                'attr' => ['class' => 'checkboxes'],
+                'label_attr' => ['class' => 'label-text'],
+                'choices' =>  [
+                    'Administrateur' => 'ROLE_ADMIN',
+                    'Manager' => 'ROLE_MANAGER',
+                    'Guild Wars 2' => 'ROLE_GW2',
+                    'Genshin Impact' => 'ROLE_GENSHIN'
+                ],
+                'expanded' => true,
+                'multiple' => true,
+            ])
+            ->add('nickname', TextType::class, [
+                'label' => 'Pseudonyme',
+                'attr' => ['class' => 'input input-bordered'],
+                'label_attr' => ['class' => 'label-text'],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer',
+                'attr' => [
+                    'class' => 'btn btn-primary'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}

287e96cd711691c4411e8a5bfb5b63c8a586e292
Anthony Destenay <anthony.destenay@gmail.com>
2023-09-09 12:00:24 +0200

    Updated palia recipes endpoint with img encoded

--- src/Controller/Api/Palia/RecipeController.php
+++ src/Controller/Api/Palia/RecipeController.php
@@ -3,18 +3,30 @@
 namespace App\Controller\Api\Palia;
 
 use App\Repository\Palia\RecipeRepository;
+use App\Service\ImageEncoded;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class RecipeController extends AbstractController
 {
     #[Route('/api/palia/recipes', name: 'app_api_palia_recipes')]
-    public function appApiPaliaItems(RecipeRepository $recipeRepository, SerializerInterface $serializer): JsonResponse
+    public function appApiPaliaItems(RecipeRepository $recipeRepository, SerializerInterface $serializer, ImageEncoded $imageEncoded): JsonResponse
     {
-        $items = $recipeRepository->findAll();
-        return new JsonResponse($serializer->serialize($items, 'json', ['groups' => ['recipe']]), 200, [], true);
+        $recipes = $recipeRepository->findAll();
+        foreach($recipes as $r) {
+            $r->getItem()->setIconEncoded($imageEncoded->get($r->getItem()->getIcon(), '/uploads/api/palia/items/', 'palia_item_icon'));
+            if(!$r->getWorkshop()->getIconEncoded()) {
+                $r->getWorkshop()->setIconEncoded($imageEncoded->get($r->getWorkshop()->getIcon(), '/uploads/api/palia/items/', 'palia_item_icon'));
+            }
+            foreach($r->getIngredients() as $i) {
+                if(!$i->getItem()->getIconEncoded()) {
+                    $i->getItem()->setIconEncoded($imageEncoded->get($i->getItem()->getIcon(), '/uploads/api/palia/items/', 'palia_item_icon'));
+                }
+            }
+
+        }
+        return new JsonResponse($serializer->serialize($recipes, 'json', ['groups' => ['recipe']]), 200, [], true);
     }
 }

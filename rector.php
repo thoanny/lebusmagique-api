ce187d8a836f127b491f7c464aeb06cd28a813d2
Anthony Destenay <anthony.destenay@gmail.com>
2025-12-11 11:09:32 +0100

    Refactored PHP73

--- rector.php
+++ rector.php
@@ -11,7 +11,7 @@ return RectorConfig::configure()
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ])
-    ->withPhpSets(php71: true)
+    ->withPhpSets(php73: true)
 //    ->withTypeCoverageLevel(0)
 //    ->withDeadCodeLevel(0)
 //    ->withCodeQualityLevel(0)
